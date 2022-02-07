1ded326f48a6b32ccdffd327a6d3fc51a42f9a42
Vinícius Cruz <vvinicruzz@gmail.com>
2022-02-07 14:32:22 -0300

    ex js chato ou sla

---
+++ ex015/script.js
@@ -0,0 +1,20 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fAno.Value == 0 || fAno.value > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - fAno.value
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
+}
