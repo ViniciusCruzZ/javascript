1dad64eb819a345cafcc9b225743d5025745b1e1
Vinícius Cruz <vvinicruzz@gmail.com>
2022-02-07 17:49:46 -0300

    att

---
+++ ex016/script.js
@@ -0,0 +1,25 @@
+function contar () {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length== 0) {
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (i < f) {
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+        for (var c = i; c >= f; c -= p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+        res.innerHTML += `\u{1F3C1}`
+    } 
+    }
+}
